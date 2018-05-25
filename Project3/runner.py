5594c343a7a97c5f9e06036e72c984fc2767f201
Iryna Havaronak <Iryna_Havaronak@epam.com>
2018-05-25 12:44:52 +0300

    Add changes in runner.py

--- Project3/runner.py
+++ Project3/runner.py
@@ -1,22 +1,30 @@
 import pytasks
+import sys
 
 
-def runner(str1=""):
-    if str1 == "":
+def runner(*args):
+    if len(args) == 0:
         print(pytasks.generate_numbers())
         print(pytasks.count_characters())
         print(pytasks.fizzbuzz())
         print(pytasks.is_palindrome())
         print(pytasks.count_characters())
-    elif str == "generate_numbers":
-        print(pytasks.generate_numbers())
-    elif str == "count_characters":
-        print(pytasks.count_characters())
-    elif str == "fizzbuzz":
-        print(pytasks.fizzbuzz())
-    elif str == "is_palindrome":
-        print(pytasks.is_palindrome())
-    elif str == "count_characters":
-        print(pytasks.count_characters())
-    elif str == "happy_numbers":
-        print(pytasks.happy_numbers())
+    for str1 in args:
+        if str1 == "generate_numbers":
+            print(pytasks.generate_numbers())
+        elif str1 == "count_characters":
+            print(pytasks.count_characters())
+        elif str1 == "fizzbuzz":
+            print(pytasks.fizzbuzz())
+        elif str1 == "is_palindrome":
+            print(pytasks.is_palindrome())
+        elif str1 == "count_characters":
+            print(pytasks.count_characters())
+        elif str1 == "happy_numbers":
+            print(pytasks.happy_numbers())
+
+
+if __name__ == "__main__":
+    arguments = sys.argv[1:]
+    for i in arguments:
+        runner(i)
