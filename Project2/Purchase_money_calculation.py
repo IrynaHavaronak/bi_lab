789ff10054a4b85008a53ec8524a4fa07968171b
Iryna Havaronak <Iryna_Havaronak@epam.com>
2018-05-25 09:33:15 +0300

    Add task 3

--- Project2/Purchase_money_calculation.py
+++
@@ -1,8 +0,0 @@
-# a program calculates total cost. One item costs M dollars and N cents.
-# Customer bought L items. Print total price in dollars and cents for L items
-dollars = int(input("Put dollars:  "))
-cents = int(input("Put cents: "))
-items = int(input("Put items: "))
-dollars = dollars * items + cents * items // 100
-cents = cents * items % 100
-print("Total cost: %d dollar(s) and %d cent(s)" % (dollars, cents))
