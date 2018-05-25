8e4b2478bfd35ae8103804f71e3d90fd407f8ffc
Iryna Havaronak <Iryna_Havaronak@epam.com>
2018-05-25 09:20:38 +0300

    Add task 3

---
+++ Project3/runner.py
@@ -0,0 +1,28 @@
+import pytasks
+
+
+def runner(str1="", str2=""):
+    if str2 != "":
+        runner(str1)
+        runner(str2)
+    if str1 == "":
+        print(pytasks.generate_numbers())
+        print(pytasks.count_characters())
+        print(pytasks.fizzbuzz())
+        print(pytasks.is_palindrome())
+        print(pytasks.count_characters())
+    elif str == "generate_numbers":
+        print(pytasks.generate_numbers())
+    elif str == "count_characters":
+        print(pytasks.count_characters())
+    elif str == "fizzbuzz":
+        print(pytasks.fizzbuzz())
+    elif str == "is_palindrome":
+        print(pytasks.is_palindrome())
+    elif str == "count_characters":
+        print(pytasks.count_characters())
+    elif str == "happy_numbers":
+        print(pytasks.happy_numbers())
+
+
+runner("happy_numbers", "is_palindrome")
